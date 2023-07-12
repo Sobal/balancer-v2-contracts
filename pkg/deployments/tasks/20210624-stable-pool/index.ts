2c332ee9d48aa767bd0f39687795575d7e877708
Ignacio Tirabasso <ignacio.tirabasso@gmail.com>
2023-07-12 10:17:58 -0300

    remove neonscan custom verifier

--- pkg/deployments/tasks/20210624-stable-pool/index.ts
+++ pkg/deployments/tasks/20210624-stable-pool/index.ts
@@ -1,16 +1,11 @@
-import Verifier from '../../../../lib/scripts/plugins/verifier';
 import Task from '../../src/task';
-import {neonscan} from "hardhat"
 import { StablePoolDeployment } from './input';
 
 export default async (task: Task, force = false): Promise<void> => {
   const output = task.output({ ensure: false });
-
   if (force || !output.factory) {
     const input = task.input() as StablePoolDeployment;
     const factory = await task.deploy('StablePoolFactory', [input.vault]);
     task.save({ factory });
-    await neonscan.verifier.verify('StablePoolFactory', factory.address, [input.vault])
-    
   }
 };
